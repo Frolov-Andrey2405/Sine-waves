5b92c094acc2a6596d1ac31504ba9f7376ba25f6
Andrey Frolov <senior.slender2015@gmail.com>
2024-04-09 10:23:16 +0300

    ruff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
 from dataclasses import dataclass, field
+
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
 from numba import jit
 
 
